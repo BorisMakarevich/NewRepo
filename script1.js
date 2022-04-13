e4d8eb6310b9f45ae9cc402ea0dc8c0c1787895a
BendaKutas <ukupnik2033@gmail.com>
2022-04-13 17:38:23 +0300

    ternarny operatorr

---
+++ script1.js
@@ -0,0 +1,43 @@
+" use strict";
+
+if (4 == 4) {
+    console.log('Ebat!!!');
+} else {
+    console.log('trubble');
+}
+
+
+// const num = 50;
+
+
+// if (num < 49) {
+//     console.log('Ebat!!!');
+// } else if(num > 100) {
+//     console.log('trubble');
+// } else   {
+//     console.log('Victory');
+// }
+//(num == 51) ? console.log('trubble') : console.log('Victory');  тернарный оператор
+  
+
+const num = 50;
+
+switch (num) {
+    case 49:
+        console.log('Ne Verno');
+        break;
+    case 49:
+        console.log('Ne Verno');
+        break;
+    case 23:
+        console.log('Ne Verno');
+        break;
+    case 59:
+        console.log('Verno');
+        break;
+    default:
+        console.log('Ne v etot raz');
+        break;
+         
+     
+}
