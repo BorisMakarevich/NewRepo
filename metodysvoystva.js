c63d08022607c3cdbf15e3832fae9c0699a815ff
BendaKutas <ukupnik2033@gmail.com>
2022-04-15 17:39:02 +0300

    genres

---
+++ metodysvoystva.js
@@ -0,0 +1,34 @@
+" use strict";
+
+
+//console.log(str.length);
+
+//const arr = [1, 2, 3];
+
+const str = "test";
+console.log(str.toLocaleUpperCase());
+console.log(str);
+
+const fruit = "Some fruit";
+console.log(fruit.indexOf("fruit"));   //5
+
+const logg = "Hello Hell";
+console.log(logg.slice(6, 10 ));    //что мы хотим вернуть из строки от индекса и по индекс
+console.log(logg.substring(6, 10 ));    //что мы хотим вернуть из строки от индекса и по индекс
+console.log(logg.substr(6, 10 ));    //что мы хотим вернуть из строки от индекса и по индекс
+
+//Math
+
+const num = 12.3;
+console.log(Math.round(num));   //  Okruglyaem
+
+const test = "12.3px";
+//console.log(parseInt(test));  //chislovoi typ dannyh
+console.log(parseFloat(test));   // chislo ili stroky v desyatichnyi typ 
+
+
+
+
+
+
+
